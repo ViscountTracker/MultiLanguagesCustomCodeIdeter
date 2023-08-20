66667d82ff40bfd9920ce86b8ccfa225d2d0aa83
Vladyslav Zaitsev <zaycev777vlad@gmail.com>
2023-08-20 17:57:33 +0300

    Ready enviroment configuration

---
+++ Service/ClientHackerEarth.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SandboxForTasks.Service
+{
+    class ClientHackerEarth
+    {
+        HttpClient httpClient;
+        private string clientId;
+        private string clientSecretKey;
+
+       public ClientHackerEarth()
+        { 
+            httpClient = new HttpClient();
+            clientId = ConfigurationManager.AppSettings["clientId"];
+            clientSecretKey = ConfigurationManager.AppSettings["clientSecretKey"];
+        }
+    }
+}
