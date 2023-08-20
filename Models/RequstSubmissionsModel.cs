01db1caf040eac5fbef65b976d9dbf1cc197bdfc
Vladyslav Zaitsev <zaycev777vlad@gmail.com>
2023-08-21 01:00:27 +0300

    Describe: Added Models for RequstSubmissionsModel and ResponseSubmissionsModel

---
+++ Models/RequstSubmissionsModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Printing;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SandboxForTasks.Models
+{
+    public class RequstSubmissionsModel
+    {
+        public string Lang { get; set; }
+        public string Source { get; set; }
+        public string Input { get; set; }
+        public int MemoryLimit { get; set; }
+        public int TimeLimit { get; set; }
+        public string Context { get; set; }
+        public string Callback { get; set; }
+
+        public RequstSubmissionsModel( string lang, string source, string input, int memoryLimit, int timeLimit, string context, string callback) 
+        {
+            Lang = lang;
+            Source = source;
+            Input = input;
+            MemoryLimit= memoryLimit;
+            TimeLimit= timeLimit;
+            Context = context;
+            Callback = callback;
+        }
+    }
+}
---
+++ Models/ResponseSubmissionsModel.cs
@@ -0,0 +1,44 @@
+﻿namespace SandboxForTasks.Models
+{
+    class ResponseSubmissionsModel
+    {
+        public RequstStatus RequestStatus { get; set; }
+        public Result Result { get; set; }
+        public string HeId { get; set; }
+        public string Context { get; set; }
+        public string StatusUrlCode { get; set; }
+
+        public ResponseSubmissionsModel(string heId, string context, string statusUrlCode, Result result, RequstStatus requstStatus ) 
+        {
+            HeId = heId;
+            Context = context;
+            StatusUrlCode = statusUrlCode;
+            RequestStatus = requstStatus;
+            Result = result;
+        }
+    }
+    public class RequstStatus
+    {
+        public string Message { get; set; }
+        public string Code { get; set; }
+        public RequstStatus(string message, string code) 
+        {
+            Message = message;
+            Code = code;
+        }
+    }
+
+    public class Result
+    {
+        public string RunStatus { get; set; }
+        public string Status { get; set; }
+        public string CompileStatus { get; set; }
+
+        public Result(string runStatus,string status, string compileStatus) 
+        {
+            RunStatus = runStatus;
+            Status = status;
+            CompileStatus=compileStatus;
+        }
+    }
+}
